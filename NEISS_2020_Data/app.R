1eb4cb672c1d1b79fe63f81ce0b8ac8be45328f8
Joey Ferkull <ferkullj@mail.gvsu.edu>
2021-11-15 23:48:49 +0000

    Tried adding data set option to Shiny app. Tried getting product labels again. Deleted some lines I'm not using any more.

--- NEISS_2020_Data/app.R
+++ NEISS_2020_Data/app.R
@@ -2,12 +2,30 @@
 library(shiny)
 library(ggplot2)
 neiss <- readr::read_csv("neiss2020.csv")
+bow <- neiss %>% 
+    subset(Age > 200) %>% 
+    mutate( Age_in_Months = Age - 200)
+aow <- neiss %>% 
+    subset(Age <200 & Age > 17)
+kow <- neiss %>%
+    subset(Age <18 & Age < 1)
 
 #define ui
 ui <- fluidPage(
     sidebarLayout(
         #Select inputs
         sidebarPanel(
+            # Select data set
+            selectInput(
+                inputId = "data_set", 
+                label = "Select Age",
+                choices = c("Under 2 years old" = "bow",
+                            "2-17 years old"    = "kow",
+                            "18 years or older" = "aow"
+                           ),
+                selected = "bow"),
+            
+            #Select x-axis
             selectInput(
                 inputId = "x",
                 label = "X-axis:",
@@ -25,7 +43,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     
     output$histogram <- renderPlot({
-        ggplot(data = neiss, aes_string(x = input$x))+
+        ggplot(data = input$data_set, aes_string(x = input$x))+
             geom_bar()
     })
     
